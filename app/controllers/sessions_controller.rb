efea9215ff7a5cd23a5892553b5dab64a3180752
pchiu <pei-hsin-chiu@uiowa.edu>
2023-11-09 03:39:19 -0600

    log out implementation in html and routes.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
+
   def destroy
     session[:session_token]=nil
     @current_user=nil
     flash[:notice]="You have been logged out."
-    redirect_to root_url
+    redirect_to 'navigation_pages/home'
   end
 end
