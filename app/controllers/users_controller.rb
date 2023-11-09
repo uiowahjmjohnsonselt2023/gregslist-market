d8dc4b17426d50b38482abd041074df60a097513
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-09 00:29:22 -0600

    User login and test for valid input complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      log_in @user
       redirect_to @user
       flash[:success] = "Welcome to Greglist Marketplace!"
       # Handle a successful save.
