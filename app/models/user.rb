8add1fc42f9452c75fde455d48e4d1eb8dd524ff
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-08 21:21:07 -0600

    User email and username downcase in user model and test implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
+  validates :username,  presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
