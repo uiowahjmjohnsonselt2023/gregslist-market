cac3dd104940338bc05a34fcad8aa049b8f595e8
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-09 15:56:10 -0600

    Remember me and seesion storage implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -18,5 +19,24 @@ class User < ActiveRecord::Base
              BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil? #if remember_digest is nil, then return false
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_032406) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_09_213807) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_032406) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
