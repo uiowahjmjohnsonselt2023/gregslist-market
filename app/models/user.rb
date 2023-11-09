01120b1871b3cb9bcb893a685f28e437e10791c3
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-08 21:24:30 -0600

    Password digest defined in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_save { self.username = username.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   validates :username,  presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_025112) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_09_031656) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
