47130255052f03caa154996709e1cd4c991f07f8
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-08 22:46:14 -0600

    Test for invalid signup implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  presence:  {message: "Name can't be blank" }, length: { maximum: 50 }
   validates :username,  presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
