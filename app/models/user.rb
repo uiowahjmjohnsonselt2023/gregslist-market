69080a3c6666b6745bb4e416bf21bea40228bcb0
Zellenon <zellenon@gmail.com>
2023-11-09 18:31:20 -0600

    the autoformatter wept

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,24 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.username = username.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :username,  presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   validates_confirmation_of :password
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
              BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+           end
+    BCrypt::Password.create(string, cost:)
   end
-
 end
+
