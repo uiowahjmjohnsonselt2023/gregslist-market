08101a75feeab543200295ca66657e56c2b1eb16
Zellenon <zellenon@gmail.com>
2023-11-09 19:13:33 -0600

    post-seller-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_032406) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_001053) do
+  create_table "sellers", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
