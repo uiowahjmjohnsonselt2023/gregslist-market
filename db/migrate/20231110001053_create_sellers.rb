6478735e42229004a34695aab591d3de1cd972e5
Zellenon <zellenon@gmail.com>
2023-11-09 19:12:13 -0600

    specified which fields can be null

--- db/migrate/20231110001053_create_sellers.rb
+++ db/migrate/20231110001053_create_sellers.rb
@@ -1,9 +1,9 @@
 class CreateSellers < ActiveRecord::Migration[7.1]
   def change
     create_table :sellers do |t|
-      t.string :name
-      t.text :description
-      t.text :address
+      t.string :name, null: false
+      t.text :description, null: true
+      t.text :address, null: true
 
       t.timestamps
     end
