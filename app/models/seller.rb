fb1eee4ba18a0244a4795e019b404b12734ca104
Zellenon <zellenon@gmail.com>
2023-11-09 19:11:56 -0600

    added minimal info

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,2 +1,7 @@
+# Table for all store page information
 class Seller < ApplicationRecord
+  has_many :user
+
+  before_save { self.address = address.downcase }
+  validates :name, presence: true, length: { maximum: 100 }
 end
