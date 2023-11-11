dda51b86102383036273c300d6e127c7d789cf37
pchiu <pei-hsin-chiu@uiowa.edu>
2023-11-10 19:56:20 -0600

    User.new_token and User.digest pass the test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,53 @@
-require "test_helper"
+require "simplecov"
+require 'test_helper'
+SimpleCov.start "rails"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_path
     assert_response :success
   end
+
+
+  test "should create a new user" do
+    assert_difference('User.count', 1) do
+      post users_path, params: { user: { name: "John Doe", username: "johndoe", email: "john@example.com", password: "password", password_confirmation: "password" } }
+    end
+
+    assert_redirected_to user_path(User.last)
+    follow_redirect!
+    assert_select 'div.alert-success', "Welcome to Greglist Marketplace!"
+  end
+
+  test "should not create a new user if blank email" do
+    assert_no_difference('User.count') do
+      post users_path, params: { user: { name: "John Doe", username: "johndoe", email: " ", password: "password", password_confirmation: "password" } }
+    end
+  end
+
+  test "digest method should create a valid BCrypt hash" do
+    # Replace 'password' with the string you want to hash for testing
+    input_string = 'password'
+
+    # Call the digest method
+    hashed_string = User.digest(input_string)
+
+    # Ensure that the result is a valid BCrypt hash
+    assert BCrypt::Password.new(hashed_string).is_password?(input_string)
+  end
+
+  test "new_token method should return a valid URL-safe base64 string" do
+    # Call the new_token method
+    token = User.new_token
+
+    # Ensure that the result is a string
+    assert_instance_of String, token
+
+    # Ensure that the token has the correct length (adjust as needed)
+    assert_equal 22, token.length
+
+    # Ensure that the token consists of URL-safe base64 characters
+    assert_match(/\A[-\w]+\z/, token)
+  end
+
 end
