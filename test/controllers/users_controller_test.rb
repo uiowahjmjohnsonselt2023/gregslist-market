b21a66831608d76d88f386bd05d4b2c5b82cb7dc
pchiu <pei-hsin-chiu@uiowa.edu>
2023-11-10 19:58:36 -0600

    remember pass the test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,11 +3,15 @@ require 'test_helper'
 SimpleCov.start "rails"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.new(name: "John Doe", username: 'johndoe', email: "johndoe@mymail.com",
+                     password: "foo@bar", password_confirmation: "foo@bar")
+  end
+
   test "should get new" do
     get new_user_path
     assert_response :success
   end
-
 
   test "should create a new user" do
     assert_difference('User.count', 1) do
@@ -48,6 +52,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     # Ensure that the token consists of URL-safe base64 characters
     assert_match(/\A[-\w]+\z/, token)
+  end
+
+  test "remember method should set remember_token and update remember_digest" do
+    # Call the remember method
+    @user.remember
+
+    # Ensure that remember_token is set
+    assert_not_nil @user.remember_token
+
+    # Ensure that remember_digest is updated
+    assert_not_nil @user.remember_digest
+    assert BCrypt::Password.new(@user.remember_digest).is_password?(@user.remember_token)
   end
 
 end
