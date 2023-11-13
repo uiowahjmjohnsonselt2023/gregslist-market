1936227d0a7f4198222d21e39788c1686a8d4875
pchiu <pei-hsin-chiu@uiowa.edu>
2023-11-13 12:40:41 -0600

    Fix the issue of uninitialized constant User. Now, features tests can used the step, "Given the following users are existing".

--- Gemfile
+++ Gemfile
@@ -58,8 +58,6 @@ gem 'turbolinks', '~> 5.2.0'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 gem "minitest"
-gem 'rails-controller-testing'
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
@@ -89,6 +87,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem 'simplecov', :require=> false
+  gem 'rails-controller-testing'
 end
 
 group :production do
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,24 +1,15 @@
-require 'uri'
-require 'cgi'
-
-module WithinHelpers
-  def with_scope(locator)
-    locator ? within(*selector_for(locator)) { yield } : yield
-  end
-end
-World(WithinHelpers)
-
 
 Given /the following users exist/ do |users_table|
   users_table.hashes.each do |user|
-    User.create!(
+    User.create(
       name: user['name'],
       username: user['username'],
-      password_digest: user['password_digest'],
+      password: user['password'],
       email: user['email']
     )
   end
 end
+
 
 When /^(?:|I )log in with email "([^"]*)" and password "([^"]*)"$/  do |email, password|
   @login_email = email
@@ -52,58 +43,25 @@ And /^(?:|I )fill in "([^"]*)"$/ do |field|
 end
 
 When("I complete the signup form") do
-  @signup_name="coconaceas"
-  @signup_username="cucum123"
-  @signup_password="ck1231KIas1"
-  @signup_password_confirmation="ck1231KIas1"
-  @signup_email="ccumt98@gmail.com"
+  @signup_name="t5name"
+  @signup_username="t5user"
+  @signup_password="t5555556"
+  @signup_password_confirmation="t5555556"
+  @signup_email="t5@gmail.com"
   fill_in "user_name", with: @signup_name
   fill_in "user_username", with: @signup_username
   fill_in "user_password", with: @signup_password
   fill_in "user_password_confirmation", with: @signup_password_confirmation
   fill_in "user_email", with: @signup_email
   click_button "Create my account"
-
-
 end
 
 When "I sign in" do
-  @login_email = "ccumt98@gmail.com"
-  @login_password = "ck1231KIas1"
+  @login_email = "t5@gmail.com"
+  @login_password = "t5555556"
   fill_in "Email", with: @login_email
   fill_in "Password", with: @login_password
   click_button "Log in"
-end
-
-Then("I should be a valid user") do
-  user=User.new(
-    name: @signup_name,
-    username: @signup_username,
-    email: @signup_email,
-    password: @signup_password
-  )
-  expect(user.valid?).to be true
-  user.save
-  expect(User.find_by(email: @signup_email)).not_to be_nil
-end
-
-Then("I should be a valid seller") do
-  seller=Seller.new(
-    name: @signup_name,
-    username: @signup_username,
-    email: @signup_email,
-    password: @signup_password,
-    password_confirmation: @signup_password_confirmation
-  )
-  expect(seller.valid?).to be true
-  seller.save
-  expect(Seller.find_by(email: @signup_email)).not_to be_nil
-end
-
-Then("I should see my user information") do
-  expect(page).to have_content(User.username)
-  expect(page).to have_content(User.password)
-  expect(page).to have_content(User.email)
 end
 
 Then /^(?:|I )should see my name "([^"]*)"$/ do |name|
@@ -112,15 +70,6 @@ end
 
 Then("I should see my new address") do
   expect(page).to have_content(@new_address)
-end
-
-And "I edit my address" do
-  user = User.find_by(@logged_in_user)
-  @new_address="456 Updated St, Iowa City, USA"
-  if user
-    user.update(address: @new_address)
-    user.save
-  end
 end
 
 Then("I should see the field for {string}") do |field_name|
