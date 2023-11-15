1cbba7b4aa54908fb329207a16cb28395aa793a0
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-15 16:56:50 -0600

    Log out bug fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,9 +43,10 @@ module SessionsHelper
     @current_user = nil
   end
 
-  def destroy
-    log_out
-    redirect_to root_url
-  end
+  # should be in sessions controller
+  # def destroy
+  #   log_out
+  #   redirect_to root_url
+  # end
 
 end
