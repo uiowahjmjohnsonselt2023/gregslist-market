6b10920da80a0d887d9e5e49a97ed5d85c24e60a
pchiu <pei-hsin-chiu@uiowa.edu>
2023-11-17 14:03:11 -0600

    Steps for complete the seller edit form and be redirect to the store page.

--- features/step_definitions/seller_steps.rb
+++ features/step_definitions/seller_steps.rb
@@ -1,4 +1,4 @@
-Given /my following stores exist/ do |sellers_table|
+Given /the following stores exist/ do |sellers_table|
   sellers_table.hashes.each do |seller|
     Seller.create(
       name: seller['name'],
@@ -8,45 +8,44 @@ Given /my following stores exist/ do |sellers_table|
   end
 end
 
+Given("the following associations exist") do |association_table|
+  association_table.hashes.each do |row|
+    user = User.find_by(id: row['user_id'])
+    seller = Seller.find_by(id: row['seller_id'])
+    seller.user << user
+  end
+end
 
 When 'I want to access my seller account' do
   click_link('Access Seller accounts')
 end
 
-Then 'I should see my existing seller accounts' do
-  stores=Seller_user.find_by(:id => 1)
-  stores.each do |store|
-    expect(page).to have_content(store.name)
-  end
-end
+# Then 'I should see my existing seller accounts' do
+#   seller =@user.sellers
+#   #seller_name = seller.name
+#   puts('user email=',@user.email)
+#   puts('seller name=',seller)
+#   stores.each do |store|
+#     print('num=',1)
+#     print('store=',store)
+#     #expect(page).to have_content(store.name)
+#   end
+# end
 
-When 'I select one of my seller account' do
-  stores=Seller_user.find_by(:id => 1)
-  @store=stores[1]
-  click_button('Select')
-end
 
-And "I should see my store's name" do
-  expect(page).to have_content(@store.name)
-end
 
 And "I don't have existing seller account yet" do
-  click_button('Create new Seller account')
+  click_link('Create new Seller account')
 end
 
 When 'I complete the seller edit form' do
-  @name = "cindy's 1 store"
-  @description = "the very first one"
-  @address = "123 road, IA"
-
-  fill_in "name", with: @name
-  fill_in "description", with: @description
-  fill_in "address", with: @address
-
+  name = "cindy's 1 store"
+  description = "the very first one"
+  address = "123 road, IA"
+  fill_in "Name", with: name
+  fill_in "Description", with: description
+  fill_in "Address", with: address
   click_button "Create store account"
-  @store=Seller.create(
-    name: @name,
-    description: @description,
-    address: @address
-  )
+  @store=Seller.find_by(name:name)
+  @store_id=@store.id
 end
