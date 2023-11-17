0c619613fd76c5bf945bc7bfac580ef5502cd74c
Zellenon <zellenon@gmail.com>
2023-11-16 18:22:37 -0600

    merge polish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_214315) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_032833) do
   create_table "sellers", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
