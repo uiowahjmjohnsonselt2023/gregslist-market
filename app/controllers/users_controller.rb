91732e666c7bdbb33916f4b0ffe59f817f0befad
Zellenon <zellenon@gmail.com>
2023-11-22 13:38:46 -0600

    formatting, spellcheck

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update] #this is a private method
-  before_action :admin_user,     only: :destroy #this is a private method
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user,   only: %i[edit update] # this is a private method
+  before_action :admin_user,     only: :destroy # this is a private method
 
   def index
     # @users = User.all
     @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to Greglist Marketplace!"
+      flash[:success] = 'Welcome to Gregslist Marketplace!'
       redirect_to @user
       # Handle a successful save.
     else
@@ -39,7 +41,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       redirect_to @user # Handle a successful update.
     else
       render 'edit'
@@ -57,24 +59,21 @@ class UsersController < ApplicationController
 
   # Confirms a logged-in user.
   def logged_in_user
-    unless logged_in?
-      store_location #defined in sessions_helper.rb
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    store_location # defined in sessions_helper.rb
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
-
-
 
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user) #current_user? is defined in sessions_helper.rb
+    redirect_to(root_url) unless current_user?(@user) # current_user? is defined in sessions_helper.rb
   end
 
   # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
-
 end
