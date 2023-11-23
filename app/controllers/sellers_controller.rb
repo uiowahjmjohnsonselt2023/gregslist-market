1bab5a742b9a0fcbd17c079122d091f2fa6eed90
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-11-23 17:18:19 -0600

    fixed seller creation bug

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -23,7 +23,7 @@ class SellersController < ApplicationController
 
   def create
     @seller = Seller.new(seller_params)
-    @seller.user << current_user
+    @seller.users << current_user
     if @seller.save
       redirect_to @seller
       puts 'SAVED'
