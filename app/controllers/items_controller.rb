79be5b611b98a24cc15a803d95cd343936759f35
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-11-24 00:17:58 -0600

    Seller id error, check seller select

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
     def new
         @item = Item.new
+        @item.seller_id = session[:seller_id] if session[:seller_id] # This might be a problem
     end
 end
