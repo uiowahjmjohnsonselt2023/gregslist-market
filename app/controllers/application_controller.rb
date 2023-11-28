9a28a47ce7f0bd55c4ee03a2323438c8cb257bc4
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-11-28 12:44:27 -0600

    Cleared old files in store

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include SessionsHelper
-
-  # def delete
-  #   render 'application/delete'
-  # end
-
-end
