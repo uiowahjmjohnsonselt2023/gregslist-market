1f858765a80b8df4df5ee2dc1d19eef0b202f9cc
Zellenon <zellenon@gmail.com>
2023-12-01 12:12:59 -0600

    formatting

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
-    belongs_to :seller
-    has_many :item_categories
-    has_many :categories, through: :item_categories
+  belongs_to :seller
+  has_many :item_categories
+  has_many :categories, through: :item_categories
+
   def category_names
     categories.map { |cat| cat.name }
   end
