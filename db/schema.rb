f29be8e7cc057be5731d33d1afeec1a470db015f
Zellenon <zellenon@gmail.com>
2023-12-01 00:20:44 -0600

    format

--- db/schema.rb
+++ db/schema.rb
@@ -10,74 +10,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_25_033926) do
-  create_table "buyers", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.text "payment_method"
-    t.text "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_231_125_033_926) do
+  create_table 'buyers', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.text 'payment_method'
+    t.text 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "buyers_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "buyer_id", null: false
-    t.index ["user_id", "buyer_id"], name: "index_buyers_users_on_user_id_and_buyer_id"
+  create_table 'buyers_users', id: false, force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'buyer_id', null: false
+    t.index %w[user_id buyer_id], name: 'index_buyers_users_on_user_id_and_buyer_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "item_categories", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_item_categories_on_category_id"
-    t.index ["item_id"], name: "index_item_categories_on_item_id"
+  create_table 'item_categories', force: :cascade do |t|
+    t.integer 'item_id', null: false
+    t.integer 'category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_item_categories_on_category_id'
+    t.index ['item_id'], name: 'index_item_categories_on_item_id'
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "seller_id", null: false
-    t.date "listing_date"
-    t.decimal "listed_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["seller_id"], name: "index_items_on_seller_id"
+  create_table 'items', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.integer 'seller_id', null: false
+    t.date 'listing_date'
+    t.decimal 'listed_price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['seller_id'], name: 'index_items_on_seller_id'
   end
 
-  create_table "sellers", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.text "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'sellers', force: :cascade do |t|
+    t.string 'name', null: false
+    t.text 'description'
+    t.text 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "sellers_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "seller_id", null: false
-    t.index ["user_id", "seller_id"], name: "index_sellers_users_on_user_id_and_seller_id"
+  create_table 'sellers_users', id: false, force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'seller_id', null: false
+    t.index %w[user_id seller_id], name: 'index_sellers_users_on_user_id_and_seller_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "admin", default: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'password_digest'
+    t.string 'remember_digest'
+    t.boolean 'admin', default: false
   end
 
-  add_foreign_key "item_categories", "categories"
-  add_foreign_key "item_categories", "items"
-  add_foreign_key "items", "sellers"
+  add_foreign_key 'item_categories', 'categories'
+  add_foreign_key 'item_categories', 'items'
+  add_foreign_key 'items', 'sellers'
 end
