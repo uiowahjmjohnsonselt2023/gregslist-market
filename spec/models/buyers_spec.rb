5de53ac88691e051db619e8d5a3ef41775f2c985
pchiu <pei-hsin-chiu@uiowa.edu>
2023-12-01 18:37:20 -0600

    100% coverage

---
+++ spec/models/buyers_spec.rb
@@ -0,0 +1,22 @@
+require './spec/rails_helper'
+
+RSpec.describe Buyer, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_length_of(:first_name).is_at_most(255) }
+
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:last_name).is_at_most(255) }
+
+    it { should validate_presence_of(:address) }
+    it { should validate_length_of(:address).is_at_most(255) }
+
+    it { should validate_presence_of(:payment_method) }
+    it { should validate_length_of(:payment_method).is_at_most(16) }
+  end
+
+  describe 'associations' do
+    it { should have_and_belong_to_many(:users) }
+  end
+
+end
