fd2c7af016e9b9710eb34a2a23a7a6a827fe65c6
Zellenon <zellenon@gmail.com>
2023-12-02 02:14:04 -0600

    added images to models for users, sellers, images

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :seller
   has_many :item_categories
   has_many :categories, through: :item_categories
+  has_one_attached :image
 
   def category_names
     categories.map { |cat| cat.name }
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -2,6 +2,7 @@
 class Seller < ApplicationRecord
   has_many :items
   has_and_belongs_to_many :users
+  has_one_attached :image
 
   before_save { self.address = address.downcase }
   validates :name, presence: true, length: { maximum: 100 }
