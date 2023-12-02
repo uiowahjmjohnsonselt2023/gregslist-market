343f5043393003b132dd9c9d701998a0361ac084
pchiu <pei-hsin-chiu@uiowa.edu>
2023-12-01 23:49:39 -0600

    41.18% coverage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in? #if logged_in? is true, then log_out
-    # flash.now[:notice] = 'You have successfully logged out.'
+    flash.now[:notice] = 'You have successfully logged out.'
     redirect_to root_url
   end
 end
--- spec/Controller/sessions_controller_spec.rb
+++ spec/Controller/sessions_controller_spec.rb
@@ -1,17 +1,42 @@
 require './spec/rails_helper'
 
+
 RSpec.describe SessionsController, type: :controller do
-  describe 'DELETE #destroy' do
-    it 'clears the session and logs the user out' do
-      # Simulate a user being logged in by creating a session token
-      session[:session_token] = 'example_session_token'
+  describe 'GET #new' do
+    context 'when the user is already logged in' do
+      let(:logged_in_user) { User.create(
+        name: 'Mike',
+        username: 'mike_doe',
+        password: 'passwordisSerect',
+        email: 'mike@gmail.com'
+      ) }
 
-      delete :destroy
+      before do
+        allow(controller).to receive(:logged_in?).and_return(true)
+        get :new
+      end
 
-      expect(session[:session_token]).to be_nil
-      expect(assigns(:current_user)).to be_nil
-      expect(flash[:notice]).to eq('You have been logged out.')
-      expect(response).to redirect_to(root_path)
+      it 'sets a flash message' do
+        expect(flash[:info]).to eq('You are already logged in.')
+      end
+
+      it 'redirects to the root path' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when the user is not logged in' do
+      before { get :new }
+
+      it 'does not set a flash message' do
+        expect(flash[:info]).to be_nil
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
     end
   end
+
+
 end
