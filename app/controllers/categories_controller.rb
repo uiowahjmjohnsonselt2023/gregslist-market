cf4b276aae65b3e05940603d731eb96164399372
Zellenon <zellenon@gmail.com>
2023-12-03 00:36:59 -0600

    categories are no longer controlled by sellers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    if params[:seller_id].present? || session[:seller_id].present?
-      session[:seller_id] = params[:seller_id] if params[:seller_id].present?
-      @seller = Seller.find_by(id: session[:seller_id])
-
-      
-      
-      if @seller.nil?
-        redirect_to root_path, alert: 'Seller not found'
-        return
-      end
-    else
-      redirect_to root_path, alert: 'Seller ID not provided'
-      return
-    end
-
     @categories = Category.all
-    # Render the appropriate view, e.g., render 'categories/index'
   end
 
   def create
@@ -27,16 +11,13 @@ class CategoriesController < ApplicationController
     else
       render :new
     end
-  
   end
 
   def new
     @seller = Seller.find_by(id: session[:seller_id])
     @category = Category.new
   end
-  
-    
-  
+
   private
 
   def category_params
