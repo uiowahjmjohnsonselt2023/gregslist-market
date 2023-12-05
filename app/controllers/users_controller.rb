c2c143f80ad79b3ef0e40f5de99fdc8fd3e4a027
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-12-04 19:05:10 -0600

    Purchase history view created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @purchase_history = Purchase.where(user_id: @user.id).order(id: :desc)
   end
 
   def new
