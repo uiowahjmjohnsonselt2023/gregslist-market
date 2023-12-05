b45aaab21a8562d8c395cd79beff73c064e5b24d
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-12-04 19:02:50 -0600

    Purchases controller created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    if params[:seller_id].present? || session[:seller_id].present?
-      session[:seller_id] = params[:seller_id] if params[:seller_id].present?
-      @seller = Seller.find_by(id: session[:seller_id])
-
-      if @seller.nil?
-        redirect_to root_path, alert: 'Seller not found'
-        return
-      end
-    else
-      redirect_to root_path, alert: 'Seller ID not provided'
-      return
-    end
     @categories = Category.all
-    # Render the appropriate view, e.g., render 'categories/index'
   end
-
 
   def create
     @seller = Seller.find_by(id: session[:seller_id])
