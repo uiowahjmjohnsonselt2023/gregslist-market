35fd7542c94182e582d2e4ce7350324092c12309
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-12-05 11:46:27 -0600

    Migration for account activation added

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_014739) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_174414) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -141,6 +140,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_014739) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
