dc734243cfa466fdcf10995fce29126acbe32f6c
pchiu <pei-hsin-chiu@uiowa.edu>
2023-12-07 15:04:45 -0600

    Given existing tables implemented.

--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -1,65 +1,92 @@
-Given /the following products exist/ do |products_table|
-  products_table.hashes.each do |product|
-    Product.create(
-      name: product['name'],
-      price: product['price'],
-      date: product['date'],
-      category: product['category']
+Given /the following items exist/ do |items_table|
+  items_table.hashes.each do |item|
+    Item.create(
+      name: item['name'],
+      description: item['description'],
+      listing_date: item['listing_date'],
+      listed_price: item['listed_price'],
+      seller_id:item['seller_id']
     )
   end
 end
 
-Then /(.*) products should exist/ do | n_seeds |
-  expect(Product.count).to eq n_seeds.to_i
+Given /the following categories exist/ do |categories_table|
+  categories_table.hashes.each do |category|
+    Category.create(
+      name: category['name']
+    )
+  end
 end
 
-When /^(?:|I )am in (.+)$/ do |page_name|
-  visit path_to(page_name)
+Given("the following associations between category and item exist") do |association_table|
+  association_table.hashes.each do |row|
+    category = Category.find_by(id: row['category_id'])
+    item = Item.find_by(id: row['item_id'])
+    item.categories << category
+  end
+end
+
+Then /(.*) products should exist/ do | n_seeds |
+  expect(Item.count).to eq n_seeds.to_i
+end
+
+When /^(?:|I )am in (.+) as the store owner$/ do |store_name|
+  store=Seller.find_by(name:store_name)
+  @store_id=store.id
+  visit "/sellers/#{@store_id}"
+end
+
+When 'I want to add an item' do
+  click_link('Add Item')
 end
 
 When("I complete the product post form") do
-  @name="Fujifilm Quicksnap 135 Flash 400 2pk Camera"
+  @name="Camera"
   @price="22.99"
-  @date="2023-11-13"
-  @category="electronics"
-  @seller="Cindy"
-  fill_in "name", with: @name
-  fill_in "price", with: @price
-  fill_in "date", with: @date
-  fill_in "category", with:@category
-  click_button "Post the product"
-  Product.create(
+  @date="2023/11/13"
+  # @category="electronics"
+  @description="Fujifilm Quicksnap 135 Flash 400 2pk"
+
+  fill_in "Name", with: @name
+  fill_in "Description", with: @description
+  fill_in "item[listing_date]", with: @date
+  # fill_in "Listing date", with: @date
+  fill_in "Listed price", with: @price
+  # fill_in "Category", with:@category
+  click_button "Create Item"
+  Item.create(
     name: @name,
-    price: @price,
-    date: @date,
-    category: @category,
-    seller_username:@seller
+    description:@description,
+    listing_date: @date,
+    listed_price: @price,
+    # category: @category,
+    seller_id:@store_id
   )
 end
 
 Then 'I should see my product in my store' do
-  the_product=Product.find_by(name:@name, price:@price, date:@date, category:@category, seller_username:@seller)
-  the_id=the_product.id
-  # TODO: path for one's store
-  expect(current_path).to eq('/users/'+the_id.to_s)
-  expect(page).to have_content(@name, price:@price, date:@date, category:@category)
+  the_item=Item.find_by(name:@name, description:@description, listed_price:@price, listing_date:@date, seller_id:@store_id)
+  expect(current_path).to eq("/sellers/#{@store_id}")
+  # puts('path=',current_path)
+  expect(page).to have_content(the_item.name)
+  expect(page).to have_content(the_item.description)
 end
 
 When /^(?:|I )update the price of "([^"]*)" with "([^"]*)"$/ do |name, new_price|
-  the_product=Product.find_by(name:name)
-  the_id=the_product.id
-  Product.update(the_id,price:new_price)
+  the_item=Item.find_by(name:name)
+  the_id=the_item.id
+  Item.update(the_id,listed_price:new_price)
 
   end
 
 Then /^(?:|I )should see "([^"]*)" with "([^"]*)"$/ do |name, new_price|
-  the_product=Product.find_by(name:name)
-  expect(the_product.price).to eq(new_price)
+  the_item=Item.find_by(name:name)
+  expect(the_item.listed_price).to eq(new_price)
 end
 
 And /^(?:|I )should not see "([^"]*)" with "([^"]*)"$/ do |name, old_price|
-  the_product=Product.find_by(name:name)
-  expect(the_product.price).not eq(old_price)
+  the_item=Item.find_by(name:name)
+  expect(the_item.listed_price).not eq(old_price)
 end
 
 
