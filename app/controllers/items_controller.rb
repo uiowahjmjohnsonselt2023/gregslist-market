c85e415dfdc2900efb694ef4ffdb28ab621bee55
Eric Ababio Anyimadu <86034604+eaanyimadu@users.noreply.github.com>
2023-12-08 15:20:11 -0600

    Checkout bug fixed, seller added to item list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,9 +19,9 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
   def create
     @item = Item.new(item_params)
