f3b7a07c1aafa11571993839ff06323d088a660b
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-12-09 14:57:05 -0600

    clean main

--- app/models/item.rb
+++
@@ -1,5 +0,0 @@
-class Item < ApplicationRecord
-    belongs_to :seller
-    has_many :item_categories
-    has_many :categories, through: :item_categories
-end
--- app/models/seller.rb
+++
@@ -1,8 +0,0 @@
-# Table for all store page information
-class Seller < ApplicationRecord
-  has_many :items
-  has_and_belongs_to_many :users
-
-  before_save { self.address = address.downcase }
-  validates :name, presence: true, length: { maximum: 100 }
-end
