b92845d671fdfaac7199217dcd9788491fb28b55
Stewart Sankey <stewart-sankey@uiowa.edu>
2023-12-10 01:32:39 -0600

    Updated heroku URL in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  # Eager load code on boot. This eager loads most of Rails and
+  config.action_mailer.default_url_options = { :host => "calm-brushlands-99710-3aef3fbd0217.herokuapp.com"}
+    # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
