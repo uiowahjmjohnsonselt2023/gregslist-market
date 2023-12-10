1a6fff70c8e937d2a31f7f6655ee0f654cc6f825
Zellenon <zellenon@gmail.com>
2023-12-10 03:27:56 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,147 +10,148 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_193310) do
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+ActiveRecord::Schema[7.1].define(version: 20_231_205_193_310) do
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "buyers", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.text "payment_method"
-    t.text "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_buyers_on_user_id"
+  create_table 'buyers', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.text 'payment_method'
+    t.text 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'user_id', null: false
+    t.index ['user_id'], name: 'index_buyers_on_user_id'
   end
 
-  create_table "cart_items", force: :cascade do |t|
-    t.integer "cart_id", null: false
-    t.integer "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  create_table 'cart_items', force: :cascade do |t|
+    t.integer 'cart_id', null: false
+    t.integer 'item_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['cart_id'], name: 'index_cart_items_on_cart_id'
+    t.index ['item_id'], name: 'index_cart_items_on_item_id'
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'carts', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "item_categories", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_item_categories_on_category_id"
-    t.index ["item_id"], name: "index_item_categories_on_item_id"
+  create_table 'item_categories', force: :cascade do |t|
+    t.integer 'item_id', null: false
+    t.integer 'category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_item_categories_on_category_id'
+    t.index ['item_id'], name: 'index_item_categories_on_item_id'
   end
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "seller_id", null: false
-    t.date "listing_date"
-    t.decimal "listed_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["seller_id"], name: "index_items_on_seller_id"
+  create_table 'items', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.integer 'seller_id', null: false
+    t.date 'listing_date'
+    t.decimal 'listed_price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['seller_id'], name: 'index_items_on_seller_id'
   end
 
-  create_table "purchases", force: :cascade do |t|
-    t.integer "cart_id", null: false
-    t.integer "user_id", null: false
-    t.decimal "total_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_purchases_on_cart_id"
-    t.index ["user_id"], name: "index_purchases_on_user_id"
+  create_table 'purchases', force: :cascade do |t|
+    t.integer 'cart_id', null: false
+    t.integer 'user_id', null: false
+    t.decimal 'total_price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['cart_id'], name: 'index_purchases_on_cart_id'
+    t.index ['user_id'], name: 'index_purchases_on_user_id'
   end
 
-  create_table "seller_reviews", force: :cascade do |t|
-    t.integer "seller_id", null: false
-    t.integer "user_id", null: false
-    t.integer "rating"
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["seller_id"], name: "index_seller_reviews_on_seller_id"
-    t.index ["user_id"], name: "index_seller_reviews_on_user_id"
+  create_table 'seller_reviews', force: :cascade do |t|
+    t.integer 'seller_id', null: false
+    t.integer 'user_id', null: false
+    t.integer 'rating'
+    t.string 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['seller_id'], name: 'index_seller_reviews_on_seller_id'
+    t.index ['user_id'], name: 'index_seller_reviews_on_user_id'
   end
 
-  create_table "sellers", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.text "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'sellers', force: :cascade do |t|
+    t.string 'name', null: false
+    t.text 'description'
+    t.text 'address'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "sellers_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "seller_id", null: false
-    t.index ["user_id", "seller_id"], name: "index_sellers_users_on_user_id_and_seller_id"
+  create_table 'sellers_users', id: false, force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'seller_id', null: false
+    t.index %w[user_id seller_id], name: 'index_sellers_users_on_user_id_and_seller_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'password_digest'
+    t.string 'remember_digest'
+    t.boolean 'admin', default: false
+    t.string 'activation_digest'
+    t.boolean 'activated', default: false
+    t.datetime 'activated_at'
+    t.string 'reset_digest'
+    t.datetime 'reset_sent_at'
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "buyers", "users"
-  add_foreign_key "cart_items", "carts"
-  add_foreign_key "cart_items", "items"
-  add_foreign_key "item_categories", "categories"
-  add_foreign_key "item_categories", "items"
-  add_foreign_key "items", "sellers"
-  add_foreign_key "purchases", "carts"
-  add_foreign_key "purchases", "users"
-  add_foreign_key "seller_reviews", "sellers"
-  add_foreign_key "seller_reviews", "users"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'buyers', 'users'
+  add_foreign_key 'cart_items', 'carts'
+  add_foreign_key 'cart_items', 'items'
+  add_foreign_key 'item_categories', 'categories'
+  add_foreign_key 'item_categories', 'items'
+  add_foreign_key 'items', 'sellers'
+  add_foreign_key 'purchases', 'carts'
+  add_foreign_key 'purchases', 'users'
+  add_foreign_key 'seller_reviews', 'sellers'
+  add_foreign_key 'seller_reviews', 'users'
 end
