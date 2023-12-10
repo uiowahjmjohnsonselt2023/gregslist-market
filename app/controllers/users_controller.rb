c72defc76c64db53468bbce66393ea3844eef6ff
Zellenon <zellenon@gmail.com>
2023-12-09 20:02:01 -0600

    removed duplicate code comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,18 +17,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     log_in @user
-  #     flash[:success] = 'Welcome to Gregslist Marketplace!'
-  #     redirect_to @user
-  #     # Handle a successful save.
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
   def create
     @user = User.new(user_params)
     if @user.save
