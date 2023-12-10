cba6fd0c16edccb0e6efb06a6144d41db9b12c0c
Zellenon <zellenon@gmail.com>
2023-12-09 18:15:44 -0600

    someone broke item indexes without search parameters

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,13 +28,9 @@ class ItemsController < ApplicationController
                Item.joins(seller: :users)
              end
     @q = params[:search] && params[:search][:q]
-    if @q.empty?
-      flash[:warning] = 'Please enter a search term'
-      redirect_to root_path
-    else
-      # return unless @q && !@items.empty?
-      @items = @items.ransack(name_i_cont: @q).result(distinct: true)
-    end
+    return unless @q && !@items.empty?
+
+    @items = @items.ransack(name_i_cont: @q).result(distinct: true)
   end
 
   def show
