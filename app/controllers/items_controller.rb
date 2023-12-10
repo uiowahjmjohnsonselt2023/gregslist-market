25483724944f8025869ace34b35ad0b3208d4a52
Zellenon <zellenon@gmail.com>
2023-12-09 20:49:48 -0600

    added search function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,20 +13,19 @@ class ItemsController < ApplicationController
              else
                Item.joins(seller: :users)
              end
+  end
+
+  def search
+    @items = if current_user&.admin
+               Item.all
+             else
+               Item.joins(seller: :users)
+             end
     @q = params[:search] && params[:search][:q]
     return unless @q && !@items.empty?
 
     @items = @items.ransack(name_i_cont: @q).result(distinct: true)
   end
-
-  def result
-    @result = Item.find(params[:id])
-  end
-
-  # def result
-  #   @item = Item.find(params[:id])
-  # end
-
 
   def show
     @item = Item.find(params[:id])
